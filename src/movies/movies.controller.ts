b98f34c88f6da694042550a0d6e1f9901705e190
chemist-echa <echa@ample.im>
2022-10-13 16:59:03 +0900

    architecture, rest api, unit testing, e2e testing

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  Req,
+  Res,
+} from '@nestjs/common';
+import { query } from 'express';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(readonly movieService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.movieService.getAll();
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') movieId: number): Movie {
+    return this.movieService.getOne(movieId);
+  }
+
+  @Post()
+  create(@Body() movieData: CreateMovieDto) {
+    return this.movieService.create(movieData);
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: number) {
+    return this.movieService.deleteOne(movieId);
+  }
+
+  @Patch('/:id')
+  path(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
+    return this.movieService.update(movieId, updateData);
+  }
+}
---
+++ src/movies/movies.service.ts
@@ -0,0 +1,41 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+import { Movie } from './entities/movie.entity';
+
+@Injectable()
+export class MoviesService {
+  private movies: Movie[] = [];
+
+  getAll(): Movie[] {
+    return this.movies;
+  }
+
+  getOne(id: number): Movie {
+    console.log(typeof id);
+    const movie = this.movies.find((movie) => movie.id === id); //parseInt(id)와 동일한 기능 +id를 대신해서 쓰는게 transform
+    if (!movie) {
+      throw new NotFoundException(`Movie with ID ${id} not found`);
+    }
+    return movie;
+  }
+
+  deleteOne(id: number) {
+    this.getOne(id);
+    this.movies = this.movies.filter((movie) => movie.id !== +id);
+  }
+
+  create(movieData) {
+    this.movies.push({
+      id: this.movies.length + 1,
+      ...movieData,
+    });
+  }
+
+  update(id: number, updateData: UpdateMovieDto) {
+    const movie = this.getOne(id);
+    this.deleteOne(id);
+    this.movies.push({ ...movie, ...updateData });
+  }
+
+  search() {}
+}
